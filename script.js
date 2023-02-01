44b304b05b7a6661256336c4de389a8b35001585
Alja Čekada <alja.cekada@gmail.com>
2023-02-01 19:38:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ function setGrid(gridSize = 16) {
     square.style.backgroundColor = "white";
     square.style.border = "1px solid lightgray";
     canvas.appendChild(square);
+
+    draw(square)
   }
 }
 
@@ -32,36 +34,6 @@ function resetDrawing() {
   clear.addEventListener('click', () => {
     setGrid()
   })
-}
-
-function setGrid(gridSize = 16) {
-  const canvas = document.querySelector(".canvas");
-  canvas.style.display = "grid";
-  canvas.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-  canvas.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-  canvas.style.width = "600px";
-  canvas.style.height = "600px";
-
-  canvas.innerHTML = '';
-
-  const display = document.querySelector('.gridSize');
-  display.textContent = `${gridSize} x ${gridSize}`;
-
-  for (let i = 0; i < gridSize * gridSize; i++) {
-    const square = document.createElement("div");
-    square.style.backgroundColor = "white";
-    square.style.border = "1px solid lightgray";
-    canvas.appendChild(square);
-
-    draw(square)
-  }
-}
-
-function getSliderValue() {
-  const slider = document.querySelector('#gridSlider');
-  slider.addEventListener("input", function () {
-    setGrid(this.value);
-  });
 }
 
 // FIX THIS !!!
