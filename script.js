facd0129c7ca9a7a2351b380a983bff860848cf9
Alja Čekada <alja.cekada@gmail.com>
2023-02-01 23:10:55 +0100

    create functions for erase drawing and toggle grid

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const canvas = document.querySelector(".canvas");
+let color = '#000000'
 
 function setGrid(gridSize = 16) {
   canvas.style.display = "grid";
@@ -14,11 +15,12 @@ function setGrid(gridSize = 16) {
 
   for (let i = 0; i < gridSize * gridSize; i++) {
     const square = document.createElement("div");
-    square.style.backgroundColor = "white";
-    square.style.border = "1px solid lightgray";
+    square.style.backgroundColor = "cornsilk";
+    square.style.border = "0.01px solid lightgray";
     canvas.appendChild(square);
 
     draw(square)
+    gridToggle(square)
   }
 }
 
@@ -29,6 +31,35 @@ function getSliderValue() {
   });
 }
 
+function chooseColor() {
+  choice = document.getElementById('color');
+  choice.addEventListener('input', () => {
+    color = choice.value
+  })
+}
+
+// FIX THIS
+function rainbowColorToggle() {
+  const rainbow = document.getElementById("rainbow");
+  let isActive = false;
+
+  rainbow.addEventListener("click", function () {
+    isActive = !isActive;
+    if (isActive) {
+      rainbow.style.backgroundColor = "#141414";
+      canvas.addEventListener('mousemove', () => {
+        let r = Math.floor((Math.random() * 255) + 1);
+        let g = Math.floor((Math.random() * 255) + 1);
+        let b = Math.floor((Math.random() * 255) + 1);
+        color = `rgb(${r},${g},${b})`;
+      })
+    } else {
+      rainbow.style.backgroundColor = "#1e1e24";
+      // COLOR SHOULD BE SAME AS CHOOSEN ONE
+    }
+  });
+}
+
 function resetDrawing() {
   let clear = document.getElementById('clear')
   clear.addEventListener('click', () => {
@@ -36,29 +67,59 @@ function resetDrawing() {
   })
 }
 
-// FIX THIS !!!
+function eraseDrawing() {
+  let erase = document.getElementById('erase')
+  erase.addEventListener('click', () => {
+    color = 'cornsilk'
+  })
+}
+
 function draw(square) {
+  chooseColor()
   let isMouseDown = false;
 
-  square.addEventListener('mousedown', () => {
+  canvas.addEventListener('mousedown', () => {
     isMouseDown = true;
-    square.style.backgroundColor = "black";
   });
 
-  square.addEventListener('mouseup', () => {
+  canvas.addEventListener('mouseup', () => {
     isMouseDown = false;
   });
 
-  square.addEventListener('mouseover', (event) => {
+  square.addEventListener('mousemove', (event) => {
     if (isMouseDown) {
-      event.target.style.backgroundColor = "black";
+      event.target.style.backgroundColor = `${color}`;
+    }
+  });
+
+  square.addEventListener('touchmove', (event) => {
+    if (isMouseDown) {
+      event.target.style.backgroundColor = `${color}`;
     }
   });
 }
 
-window.onload = () => {
+function gridToggle(square) {
+  const grid = document.getElementById('grid')
+  let isActive = false;
+  grid.addEventListener('click', () => {
+    isActive = !isActive;
+    if (isActive) {
+      grid.style.backgroundColor = "#141414";
+      square.style.border = "0";
+    } else {
+      grid.style.backgroundColor = "#1e1e24";
+      square.style.border = "0.01px solid lightgray";
+    }
+  })
 
+}
+
+window.onload = () => {
   setGrid()
   getSliderValue()
   resetDrawing()
+  eraseDrawing()
+  rainbowColorToggle()
+  gridToggle()
 }
